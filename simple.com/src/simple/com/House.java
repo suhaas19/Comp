3690dcbe18f0f6f2ca04e9b0a714d8af053aec6e
suhaas19 <suhaas.nagaralla@gmail.com>
2020-02-17 22:07:38 +0530

    first commit

---
+++ simple.com/src/simple/com/House.java
@@ -0,0 +1,23 @@
+package simple.com;
+import java.util.Scanner;
+
+public class House {
+
+	public static void main(String args[]) {
+		
+	}
+	
+}
+
+class Hh{
+	int total,mat_std,tot_area,Type;
+	Scanner sc=new Scanner(System.in);
+	{
+	mat_std=sc.nextInt();
+	tot_area=sc.nextInt();
+	Type=sc.nextInt();
+	total=tot_area*Type;
+    sc.close();
+}
+}
+
---
+++ simple.com/src/simple/com/Simp.java
@@ -0,0 +1,30 @@
+package simple.com;
+import java.util.Scanner;
+
+class a
+{
+	Double pr,roi,sim,comp,time;	
+Scanner sc=new Scanner(System.in);
+
+{
+
+pr=sc.nextDouble();
+roi=sc.nextDouble();
+time=sc.nextDouble();
+sim=(pr*time*roi)/100;
+comp=pr;
+sc.close();
+}
+
+
+}
+
+public class Simp
+{
+	public static void main(String s[])
+	{
+	
+	}
+	
+	
+}
